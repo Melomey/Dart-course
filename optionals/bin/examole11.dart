58f79da24b87161e14ae3f2c25eefc746808cfe1
Melomey <samuelmelomey@gmail.com>
2024-09-01 22:53:58 +0000

    unwrapping multiple optionals

--- optionals/bin/examole11.dart
+++ optionals/bin/examole11.dart
@@ -1,1 +1,19 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  print(getFullName('emma', 'JOhn'));
+}
+
+String getFullName(
+  String firstName,
+  String lastName,
+) =>
+    withAll(
+      [firstName, lastName],
+      (names) => names.join(' '),
+    ) ??
+    'Empty';
+
+T? withAll<T>(
+  List<T> optionals,
+  T Function(List<T>) callback,
+) =>
+    optionals.any((e) => e == null) ? null : callback(optionals.cast<T>());
